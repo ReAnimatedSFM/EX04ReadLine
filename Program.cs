1092048cffe23bfbba00c7b4e278473d2656b918
William Johannes Saaby Porsmose <will5606@edu.campusvejle.dk>
2021-08-11 09:44:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace EX04ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Print
+            Console.WriteLine();
+            Console.WriteLine("Gang to tal");
+            Console.WriteLine();
+
+           //Variabler
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+
+
+            //Print
+            Console.WriteLine();
+            Console.WriteLine($"{x} * {y} = {x * y}");
+
+
+
+                
+
+        
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
